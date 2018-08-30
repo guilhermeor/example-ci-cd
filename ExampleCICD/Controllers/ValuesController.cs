6f2eda7b9f20b302ddd7ea563be2116498794189
Guilherme Oliveira Da Rosa <gorosa@stefanini.com>
2018-08-30 09:33:10 -0300

    fix controller

--- ExampleCICD/Controllers/ValuesController.cs
+++ ExampleCICD/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ExampleCICD.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "valueok", "value2" };
+            return new string[] { "valueok", "value123" };
         }
 
         // GET api/values/5
